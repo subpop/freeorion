e6a37d0225df1a2a324e91062190e6114d32fda0
LGM-Doyle <LGM-Doyle@users.noreply.github.com>
2016-05-06 14:30:49 -0400

    C++ sampler: Mark overridden virtual functions with override in SDLGUI.h
    The override keyword explicitly marks a virtual function as an overidden
    function in derived class.  Compilation will fail if this is not true.
    The virtual keyword previously used was more of a visual hint to indicate that
    a function was overridden but didn't serve any further purpose.

--- GG/GG/SDL/SDLGUI.h
+++ GG/GG/SDL/SDLGUI.h
@@ -89,38 +89,36 @@ public:
     explicit SDLGUI(int w = 1024, int h = 768, bool calc_FPS = false, const std::string& app_name = "GG",
                     int x = SDL_WINDOWPOS_UNDEFINED, int y = SDL_WINDOWPOS_UNDEFINED, bool fullscreen = false,
                     bool fake_mode_change = false); ///< ctor
-    virtual ~SDLGUI();
+    ~SDLGUI() override;
     //@}
 
     /** \name Accessors */ ///@{
-    virtual X               AppWidth() const;
-    virtual Y               AppHeight() const;
-    virtual unsigned int    Ticks() const;
-    virtual bool            Fullscreen() const;
-    virtual bool            FakeModeChange() const;
-    virtual std::string     ClipboardText() const;
+    X               AppWidth() const override;
+    Y               AppHeight() const override;
+    unsigned int    Ticks() const override;
+    virtual bool    Fullscreen() const;
+    virtual bool    FakeModeChange() const;
+    std::string     ClipboardText() const override;
     //@}
 
     /** \name Mutators */ ///@{
     void            operator()();      ///< external interface to Run()
-    virtual void    Exit(int code);
+    void            Exit(int code) override;
 
     void            SetWindowTitle(const std::string& title);
     void            SetVideoMode(X width, Y height, bool fullscreen, bool fake_mode_change);
-    virtual bool    SetClipboardText(const std::string& text);
+    bool            SetClipboardText(const std::string& text) override;
     //@}
 
     static SDLGUI*  GetGUI();                             ///< allows any code to access the gui framework by calling SDLGUI::GetGUI()
     GG::Key         GGKeyFromSDLKey(const SDL_Keysym& key); ///< gives the GGKey equivalent of key
 
-    virtual void    Enter2DMode();
-    virtual void    Exit2DMode();
+    void            Enter2DMode() override;
+    void            Exit2DMode() override;
 
-    // \override
-    virtual std::vector<std::string> GetSupportedResolutions() const;
+    std::vector<std::string> GetSupportedResolutions() const override;
 
-    // \override
-    virtual Pt      GetDefaultResolution (int display_id);
+    Pt              GetDefaultResolution (int display_id) override;
     static  Pt      GetDefaultResolutionStatic(int display_id);
     static int      NumVideoDisplaysStatic();
     virtual bool    FramebuffersAvailable() const;
@@ -139,17 +137,17 @@ protected:
     virtual void    GLInit();         ///< allows user to specify OpenGL initialization code; called at the end of SDLInit()
     virtual void    Initialize() = 0; ///< provides one-time gui initialization
 
-    virtual void    HandleSystemEvents();
+    void            HandleSystemEvents() override;
     virtual void    HandleNonGGEvent(const SDL_Event& event); ///< event handler for all SDL events that are not GG-related
 
-    virtual void    RenderBegin();
-    virtual void    RenderEnd();
+    void            RenderBegin() override;
+    void            RenderEnd() override;
 
     // these are called at the end of the gui's execution
     virtual void    FinalCleanup();   ///< provides one-time gui cleanup
     virtual void    SDLQuit();        ///< cleans up SDL and (if used) FE
 
-    virtual void    Run();
+    void            Run() override;
 
     void            ResetFramebuffer(); ///< Resizes or deletes the framebuffer for fake fullscreen.
 
